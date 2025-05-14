d6fd56fbc2dfb4a5150db496339e84299a228d0f
Rahul Kumar <88238469+notinrange@users.noreply.github.com>
2025-05-14 12:49:52 +0530

    backend url changed

--- content.js
+++ content.js
@@ -63,7 +63,7 @@ function injectButton() {
             button.disabled = true;
 
             const emailContent = getEmailContent();
-            const response = await fetch('http://localhost:8080/api/email/generate', {
+            const response = await fetch('https://shaky-cycles-repeat.loca.lt/api/email/generate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
